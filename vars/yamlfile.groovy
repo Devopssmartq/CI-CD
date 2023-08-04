b4017699412ded6aa3a1f22f66567c5317de0823
Yashwanth KP <yashwanth.kp@thesmartq.com>
2023-08-04 09:33:46 +0000

    yamlfile.groovy edited online with Bitbucket

--- vars/yamlfile.groovy
+++ vars/yamlfile.groovy
@@ -1,24 +1,24 @@
-//def call(body){
+def call(String repoUrl){
 
- // pipeline {
-   // agent {
-     // kubernetes {
-       // label 'sample-app'
-        //defaultContainer 'jnlp'
-      //yaml '''
-    //  apiVersion: v1
-      //kind: Pod
-   //   spec:
-     //   containers:
-     // - name: golang
-      //  image: golang:latest
-      //  command:
-      //  - sleep
-      //  args:
-      //  - 99d
-      //  tty: true
-      //  '''
-     // }
-   // }
- // }
-//}
+  pipeline {
+    agent {
+      kubernetes {
+        label 'sample-app'
+        defaultContainer 'jnlp'
+      yaml '''
+      apiVersion: v1
+      kind: Pod
+      spec:
+        containers:
+      - name: golang
+        image: golang:latest
+        command:
+        - sleep
+        args:
+        - 99d
+        tty: true
+        '''
+      }
+    }
+  }
+}
