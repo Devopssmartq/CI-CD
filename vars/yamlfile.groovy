3e8929c7cd96ef746abdb291af54e13dc12ae44c
Yashwanth KP <yashwanth.kp@thesmartq.com>
2023-08-04 11:19:04 +0000

    yamlfile.groovy edited online with Bitbucket

--- vars/yamlfile.groovy
+++ vars/yamlfile.groovy
@@ -1,27 +1,28 @@
 def call(body) {
-  def pipelineParams= [:]
-  body.resolveStrategy = Closure.DELEGATE_FIRST
-  body.delegate = pipelineParams
-  body()
+    def pipelineParams= [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = pipelineParams
+    body()
 
-  pipeline {
-    agent {
-        kubernetes {
-            label 'jenkins-private-cluster'
-            defaultContainer 'jnlp'
-            yaml '''
-              apiVersion: v1
-              kind: Pod
-              spec:
-                containers:
-                - name: gcloud
-                  image: google/cloud-sdk:latest
-                  command:
-                  - sleep
-                  args:
-                  - 99d
-                  tty: true
-            '''
+    pipeline {
+        agent {
+            kubernetes {
+                label 'jenkins-private-cluster'
+                defaultContainer 'jnlp'
+                yaml '''
+                apiVersion: v1
+                kind: Pod
+                spec:
+                    containers:
+                    - name: gcloud
+                    image: google/cloud-sdk:latest
+                    command:
+                    - sleep
+                    args:
+                    - 99d
+                    tty: true
+                '''
+            }
         }
     }
-}
+}    
