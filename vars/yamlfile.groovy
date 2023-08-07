b4b15fbd8f44a2d71d74a173aa0ce0af6f3cf42f
Yashwanth KP <yashwanth.kp@thesmartq.com>
2023-08-07 10:17:59 +0000

    yamlfile.groovy edited online with Bitbucket

--- vars/yamlfile.groovy
+++ vars/yamlfile.groovy
@@ -1,22 +1,32 @@
 def generatePodYaml() {
     return """
-		apiVersion: v1
-		kind: Pod
-		spec:
-		  containers:
-		  - name: gcloud
-			image: google/cloud-sdk:latest
-			command:
-			- sleep
-			args:
-			- 99d
-			tty: true
-		  - name: net
-			image: mcr.microsoft.com/dotnet/sdk:latest
-			command:
-			- sleep
-			args:
-			- 99d
-			tty: true
+		pipeline {
+    		agent {
+        		kubernetes {
+            		//label 'jenkins-private-cluster-0'
+            		defaultContainer 'jnlp'
+            		yaml '''
+                		apiVersion: v1
+                		kind: Pod
+                		spec:
+                		containers:
+                		- name: gcloud
+                    	  image: google/cloud-sdk:latest
+                    	  command:
+                    	  - sleep
+                          args:
+                    	  - 99d
+                    	  tty: true
+                          - name: net
+                    	  image: mcr.microsoft.com/dotnet/sdk:latest
+                    	  command:
+                          - sleep
+                          args:
+                          - 99d
+                          tty: true
+                    '''
+               	}
+    		}
+		}    
 	"""
 }
