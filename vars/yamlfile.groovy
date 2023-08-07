547cbe9284bff36ae5f33d545a120336fe6d95c1
Yashwanth KP <yashwanth.kp@thesmartq.com>
2023-08-07 07:40:31 +0000

    yamlfile.groovy edited online with Bitbucket

--- vars/yamlfile.groovy
+++ vars/yamlfile.groovy
@@ -1,31 +1,22 @@
-def call() {
-            "yaml '''
-        apiVersion: v1
-        kind: Pod
-        environment:
-            name: "NODE_OPTIONS"
-            value: "--max-old-space-size=4096"
-        spec:
-          containers:
-          - name: jnlp  
-            resources:
-              requests:
-                memory: "2048Mi"
-                cpu: "500m"
-              limits:
-                memory: "4096Mi"
-                cpu: "1000m"
-                
-          - name: flutter
-            image: instrumentisto/flutter:latest
-            command:
-            - cat
-            tty: true
-
-          - name: gcloud
-            image: google/cloud-sdk:latest
-            command:
-            - cat
-            tty: true
-        '''  "      
-}    
+def generatePodYaml() {
+    return """
+		apiVersion: v1
+		kind: Pod
+		spec:
+		  containers:
+		  - name: gcloud
+			image: google/cloud-sdk:latest
+			command:
+			- sleep
+			args:
+			- 99d
+			tty: true
+		  - name: net
+			image: mcr.microsoft.com/dotnet/sdk:latest
+			command:
+			- sleep
+			args:
+			- 99d
+			tty: true
+	"""
+}
