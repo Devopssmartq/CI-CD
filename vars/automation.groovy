5ebe87fed0be7d9721edc93bf52a9d9fed715f4a
Yashwanth KP <yashwanth.kp@thesmartq.com>
2023-08-11 09:50:19 +0000

    automation.groovy edited online with Bitbucket

--- vars/automation.groovy
+++ vars/automation.groovy
@@ -1,3 +1,3 @@
 def call(String name = 'User') {
-		sh "katalonc -noSplash -runMode=console -projectPath='/home/jenkins/agent/workspace/Katalona-test/My First Web UI Project.prj' -retry=0 -testSuitePath='Test Suites/simpleloginsuite' -browserType="${name}" -executionProfile='default' -apiKey=657a0519-3dae-4354-a9ce-55de5167d269' -orgID=41312 -licenseRelease=true --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true -webui.autoUpdateDrivers=true"
+		sh "katalonc -noSplash -runMode=console -projectPath='/home/jenkins/agent/workspace/Katalona-test/My First Web UI Project.prj' -retry=0 -testSuitePath='Test Suites/simpleloginsuite' -browserType='${name}' -executionProfile='default' -apiKey=657a0519-3dae-4354-a9ce-55de5167d269' -orgID=41312 -licenseRelease=true --config -proxy.auth.option=NO_PROXY -proxy.system.option=NO_PROXY -proxy.system.applyToDesiredCapabilities=true -webui.autoUpdateDrivers=true"
 }
