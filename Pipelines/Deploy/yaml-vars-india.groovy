86bd25a0bff4f2dc1b5b9f69bc39d919143a619e
vrp63531 <venkateswarareddi.p@thesmartq.com>
2023-09-22 15:55:00 +0530

    set yaml variables

--- Pipelines/Deploy/yaml-vars-india.groovy
+++ Pipelines/Deploy/yaml-vars-india.groovy
@@ -4,7 +4,9 @@ import groovy.transform.Field
 
 @Field def CONTROLDESK_YAML;  
 @Field def PWA_YAML; 
-@Field def FOODBOOK_YAML;  
+@Field def FOODBOOK_YAML;
+@Field def TIME2EAT_YAML;
+@Field def BAWEB_YAML;  
 @Field def WEBORT_YAML;
 
 @Field def APP_SERVICE_YAML;  
@@ -21,23 +23,54 @@ import groovy.transform.Field
 
 def setProperties() {
     GCS_BUCKET_NAME = "sqinternational-cicd.appspot.com"
-    ENVIRONMENT = "SPRINT-DEMO" //"${params.RELEASE_SCOPE}" == "sprint" ? "SPRINT-DEMO" : "${params.RELEASE_SCOPE}" == "preprod" ? "RELEASE" : "MASTER"
-    
-    CONTROLDESK_YAML = 'app.yaml'
-    PWA_YAML = 'smartq-pwa.yaml'
-    FOODBOOK_YAML = 'foodbook.yaml'
-    WEBORT_YAML = 'app.yaml'
+    ENVIRONMENT = RELEASE_SCOPE == "sprint" ? "SPRINT-DEMO" : RELEASE_SCOPE == "preprod" ? "RELEASE" : "MASTER"
+    /***************** INDIA deployment *********/
+//frontend service  yamls
+    CONTROLDESK_YAML = RELEASE_SCOPE == "beta" ? 'beta-app.yaml' : 'app.yaml'
 
-    APP_SERVICE_YAML = 'time2eat_appservice.yaml'
-    DEFAULT_SERVICE_YAML = 'app.yaml'
-    BACKGROUND_YAML = 'background.yaml'
-    TASKQSERVICE_YAML = 'taskqservice.yaml'
-    REPORTS_YAML = 'reports.yaml'
-    BQREPORTS_YAML = 'bqreports.yaml'
+    PWA_YAML = RELEASE_SCOPE == "beta" ? 'smartqPWABeta.yaml' : 'smartq-pwa.yaml'
+    FOODBOOK_YAML = RELEASE_SCOPE == "beta" ? '' : 'foodbook.yaml'
 
-    PY3_APPMS_YAML = "${params.APP_ENGINE_PROJECT_ID}.yaml"
-    PY3_DASHBOARD_YAML = "${params.APP_ENGINE_PROJECT_ID}.yaml"
-    PY3_SSO_YAML = 'app.yaml'
-    PY3_PRICEBOOK_YAML = 'app_db_demo.yaml'
+    TIME2EAT_YAML = ''
+    BAWEB_YAML = ''
+
+    WEBORT_YAML = RELEASE_SCOPE == "beta" ? 'beta_app.yaml' : 'app.yaml'
+//py2 service yamls
+    APP_SERVICE_YAML = RELEASE_SCOPE == "beta" ? '' : 'time2eat_appservice.yaml'
+    DEFAULT_SERVICE_YAML = RELEASE_SCOPE == "beta" ? 'app_betaapp.yaml' : 'app.yaml'
+    BACKGROUND_YAML = RELEASE_SCOPE == "beta" ? 'background_betaapp.yaml' : 'background.yaml'
+    TASKQSERVICE_YAML = RELEASE_SCOPE == "beta" ? 'taskqservice_betaapp.yaml' : 'taskqservice.yaml'
+    REPORTS_YAML = RELEASE_SCOPE == "beta" ? '' : 'reports.yaml'
+    BQREPORTS_YAML = RELEASE_SCOPE == "beta" ? 'bqreports_betaapp.yaml' : 'bqreports.yaml'
+//py3 service yamls
+    PY3_APPMS_YAML = RELEASE_SCOPE == "beta" ? "betaapp-${params.APP_ENGINE_PROJECT_ID}.yaml" : "${params.APP_ENGINE_PROJECT_ID}.yaml"
+    PY3_DASHBOARD_YAML = RELEASE_SCOPE == "beta" ? "betaapp-${params.APP_ENGINE_PROJECT_ID}.yaml" : "${params.APP_ENGINE_PROJECT_ID}.yaml"
+    PY3_SSO_YAML = RELEASE_SCOPE == "beta" ? '' : 'app.yaml'
+    PY3_PRICEBOOK_YAML = RELEASE_SCOPE == "beta" ? '' : 'app_db_demo.yaml'
+
+    // def isBeta = RELEASE_SCOPE == "beta" ? true : false   // Replace this with your actual condition
+
+    // // Use an if-else construct to set the value of 'result' based on the condition
+    // if (isBeta) {
+    //         echo "Release scope - $RELEASE_SCOPE"
+    //         CONTROLDESK_YAML = 'beta-app.yaml'
+    //         PWA_YAML = 'smartqPWABeta.yaml'
+    //         FOODBOOK_YAML = ''
+    //         TIME2EAT_YAML = ''
+    //         BAWEB_YAML = ''
+    //         WEBORT_YAML = 'beta_app.yaml'
+
+    //         APP_SERVICE_YAML = ''
+    //         DEFAULT_SERVICE_YAML = 'app_betaapp.yaml'
+    //         BACKGROUND_YAML = 'background_betaapp.yaml'
+    //         TASKQSERVICE_YAML = 'taskqservice_betaapp.yaml'
+    //         REPORTS_YAML = ''
+    //         BQREPORTS_YAML = 'bqreports_betaapp.yaml'
+
+    //         PY3_APPMS_YAML = "betaapp-${params.APP_ENGINE_PROJECT_ID}.yaml"
+    //         PY3_DASHBOARD_YAML = "betaapp-${params.APP_ENGINE_PROJECT_ID}.yaml"
+    //         PY3_SSO_YAML = ''
+    //         PY3_PRICEBOOK_YAML = ''
+    // }
 }
 return this
--- Pipelines/Deploy/yaml-vars-international.groovy
+++ Pipelines/Deploy/yaml-vars-international.groovy
@@ -4,7 +4,9 @@ import groovy.transform.Field
 
 @Field def CONTROLDESK_YAML;  
 @Field def PWA_YAML; 
-@Field def FOODBOOK_YAML;  
+@Field def FOODBOOK_YAML;
+@Field def TIME2EAT_YAML;
+@Field def BAWEB_YAML;  
 @Field def WEBORT_YAML;
 
 @Field def APP_SERVICE_YAML;  
@@ -21,23 +23,28 @@ import groovy.transform.Field
 
 def setProperties() {
     GCS_BUCKET_NAME = "sqinternational-cicd.appspot.com"
-    ENVIRONMENT = "MASTER"
+    ENVIRONMENT = RELEASE_SCOPE == "sprint" ? "SPRINT-DEMO" : RELEASE_SCOPE == "preprod" ? "RELEASE" : "MASTER"
+     /***************** INTERNATIONAL deployment *********/
+//frontend service  yamls
+    CONTROLDESK_YAML = RELEASE_SCOPE == "beta" ? 'beta-app.yaml' : 'app.yaml'
     
-    CONTROLDESK_YAML = 'app.yaml'
-    PWA_YAML = 'smartq-pwa.yaml'
-    FOODBOOK_YAML = 'foodbook.yaml'
-    WEBORT_YAML = 'app.yaml'
+    PWA_YAML = ''// not for INTERNATIONAL. for india only
+    FOODBOOK_YAML = ''// not for INTERNATIONAL. for india only
+    TIME2EAT_YAML = RELEASE_SCOPE == "beta" ? '' : 'time2eat.yaml'
+    BAWEB_YAML = RELEASE_SCOPE == "beta" ? '' : 'baweb.yaml'
 
-    APP_SERVICE_YAML = 'time2eat_appservice.yaml'
-    DEFAULT_SERVICE_YAML = 'app.yaml'
-    BACKGROUND_YAML = 'background.yaml'
-    TASKQSERVICE_YAML = 'taskqservice.yaml'
-    REPORTS_YAML = 'reports.yaml'
-    BQREPORTS_YAML = 'bqreports.yaml'
-
-    PY3_APPMS_YAML = "${params.APP_ENGINE_PROJECT_ID}.yaml"
-    PY3_DASHBOARD_YAML = "${params.APP_ENGINE_PROJECT_ID}.yaml"
-    PY3_SSO_YAML = 'app.yaml'
-    PY3_PRICEBOOK_YAML = 'app_db_demo.yaml'
+    WEBORT_YAML = RELEASE_SCOPE == "beta" ? 'beta_app.yaml' : 'app.yaml'
+//py2 service yamls
+    APP_SERVICE_YAML = RELEASE_SCOPE == "beta" ? '' : 'time2eat_appservice.yaml'
+    DEFAULT_SERVICE_YAML = ''// not for INTERNATIONAL. for india only
+    BACKGROUND_YAML = RELEASE_SCOPE == "beta" ? 'background_betaapp.yaml' : 'background.yaml'
+    TASKQSERVICE_YAML = RELEASE_SCOPE == "beta" ? 'taskqservice_betaapp.yaml' : 'taskqservice.yaml'
+    REPORTS_YAML = ''// not for INTERNATIONAL. for india only
+    BQREPORTS_YAML = RELEASE_SCOPE == "beta" ? 'bqreports_betaapp.yaml' : 'bqreports.yaml'
+//py3 service yamls
+    PY3_APPMS_YAML = RELEASE_SCOPE == "beta" ? "betaapp-${params.APP_ENGINE_PROJECT_ID}.yaml" : "${params.APP_ENGINE_PROJECT_ID}.yaml"
+    PY3_DASHBOARD_YAML = RELEASE_SCOPE == "beta" ? "betaapp-${params.APP_ENGINE_PROJECT_ID}.yaml" : "${params.APP_ENGINE_PROJECT_ID}.yaml"
+    PY3_SSO_YAML = RELEASE_SCOPE == "beta" ? '' : 'app.yaml'
+    PY3_PRICEBOOK_YAML = ''// not for INTERNATIONAL. for india only
 }
 return this
