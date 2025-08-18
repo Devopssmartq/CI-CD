645f8474a872d2a30ebd9b0f21ecb6f3b5aca505
Devopssmartq <yashwanth.kp@thesmartq.com>
2025-08-18 11:33:23 +0530

    Added some serivces to india pipeline

--- Pipelines/Deploy/config-variables-india.groovy
+++ Pipelines/Deploy/config-variables-india.groovy
@@ -46,6 +46,8 @@ def setProperties() {
     TIME2EAT_FLUTTER_YAML = ''
     //py2 service yamls
     APP_SERVICE_YAML = RELEASE_SCOPE == "beta" ? '' : 'app.yaml'//for india ::: // for international 'time2eat_appservice.yaml'
+    APP_SERVICE_YAML_PH = RELEASE_SCOPE == "beta" ? '' : 'time2eat_appservice.yaml'//for india ::: // for international 'time2eat_appservice.yaml'
+
     DEFAULT_SERVICE_YAML = RELEASE_SCOPE == "beta" ? 'app_betaapp.yaml' : (RELEASE_SCOPE == "sprint") ? "app_demo.yaml" : (RELEASE_SCOPE == "preprod") ? "app_preprod.yaml" : (RELEASE_SCOPE == "hot-fix" && GIT_BRANCH == "auto-release") ? "app_preprod.yaml" : 'app.yaml'
     BACKGROUND_YAML = RELEASE_SCOPE == "beta" ? 'background_betaapp.yaml' : 'background.yaml'
     TASKQSERVICE_YAML = RELEASE_SCOPE == "beta" ? 'taskqservice_betaapp.yaml' : 'taskqservice.yaml'
